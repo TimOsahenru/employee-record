4de50318f77f4d30e0af84fd62832a0fea54f47a
timosahenru <timosahenru@gmail.com>
2023-08-01 00:58:59 +0000

    read and get all departments

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from app.models import Department
+from rest_framework.response import Response
+from app.serializers import DepartmentSerializer
 
 
 @api_view(["GET", "POST"])
 def get_or_create_department(request):
-    pass
+    if request.method == "GET":
+        departments = Department.objects.all()
+        serializer = DepartmentSerializer(departments, many=True)
+        return Response(serializer.data)
